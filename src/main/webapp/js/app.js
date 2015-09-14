80d1064e540d829e53a35b4c254adb6588108485
Rodrigo Vega <rvega@turix.com.ar>
2015-09-14 15:47:33 -0300

    Projects form

--- src/main/webapp/js/app.js
+++ src/main/webapp/js/app.js
@@ -2,12 +2,9 @@
 var app = angular.module('tilo', [
   'ngSanitize', 
   'ui.router',
-  'tilo.dashboard'
+  'tilo.dashboard',
+  'tilo.projects'
 ])
 .config(function ($stateProvider, $urlRouterProvider) {
 	    $urlRouterProvider.otherwise('dashboard');
 });
-/*.run(["$templateCache", function($templateCache) {
-	var options = "<a href='' ng-click=''>Create new</a>";
-	$templateCache.put("bootstrap/choices.tpl.html","<ul class=\"ui-select-choices ui-select-choices-content ui-select-dropdown dropdown-menu\" role=\"listbox\" ><li class=\"ui-select-choices-group\" id=\"ui-select-choices-{{ $select.generatedId }}\"><div class=\"divider\" ng-show=\"$select.isGrouped && $index > 0\"></div><div ng-show=\"$select.isGrouped\" class=\"ui-select-choices-group-label dropdown-header\" ng-bind=\"$group.name\"></div><div id=\"ui-select-choices-row-{{ $select.generatedId }}-{{$index}}\" class=\"ui-select-choices-row\" ng-class=\"{active: $select.isActive(this), disabled: $select.isDisabled(this)}\" role=\"option\"><a href=\"javascript:void(0)\" class=\"ui-select-choices-row-inner\"></a></div></li>"+options+"</ul>");
-}]);*/
